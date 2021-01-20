6451e27978c822dd5e025acd6e40ac9429d1ff6f
flaurensia05 <62590379+flaurensia05@users.noreply.github.com>
2021-01-20 09:16:42 +0700

    Create Peminjam.java

---
+++ Peminjam.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peminjamanperpustakaan.oop;
+
+import java.util.ArrayList;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
