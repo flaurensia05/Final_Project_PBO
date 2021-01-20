3144796ed2be3d220e92de0333a35ff3096a39fc
diahsriatna25 <62596025+diahsriatna25@users.noreply.github.com>
2021-01-20 09:09:17 +0700

    Create DataPinjam.java

---
+++ DataPinjam.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peminjamanperpustakaan.oop;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ *
+ * @author achieto
+ */
+public class DataPinjam {
+    IntegerProperty IDBuku;
+    StringProperty NamaBuku;
+    StringProperty TanggalPinjam;
