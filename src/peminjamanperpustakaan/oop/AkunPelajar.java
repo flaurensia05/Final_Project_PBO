0bcc95e8cfb57ed0b41e65b4ad73dc048f6226eb
flaurensia05 <62590379+flaurensia05@users.noreply.github.com>
2021-01-20 18:54:56 +0700

    Update AkunPelajar.java

--- src/peminjamanperpustakaan/oop/AkunPelajar.java
+++ src/peminjamanperpustakaan/oop/AkunPelajar.java
@@ -9,10 +9,6 @@ import java.util.ArrayList;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
-/**
- *
- * @author achieto
- */
 public class AkunPelajar extends Peminjam{
     StringProperty NamaSekolah;
 
