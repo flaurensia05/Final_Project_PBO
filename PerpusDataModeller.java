356c2cd14de67f9156990a8a2c39b1ef28a8612a
diahsriatna25 <62596025+diahsriatna25@users.noreply.github.com>
2021-01-20 09:29:25 +0700

    Create PerpusDataModeller.java

---
+++ PerpusDataModeller.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peminjamanperpustakaan.oop;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import peminjamanperpustakaan.oop.db.DBHelper;
